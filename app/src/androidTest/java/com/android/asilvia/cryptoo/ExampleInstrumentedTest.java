67bcf05cc787c68d9e219c8b2824166ef2f8c73d
asilvia <asilvia3@gmail.com>
2018-02-28 20:12:22 +0100

    fix package name

--- app/src/androidTest/java/com/android/asilvia/cryptoo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/android/asilvia/cryptoo/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.android.asilvia.softcoin;
+package com.android.asilvia.cryptoo;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.android.asilvia.softcoin", appContext.getPackageName());
+        assertEquals("com.android.asilvia.cryptoo", appContext.getPackageName());
     }
 }
