daee6158582174e90a5200b419093bc78c21ebae
Ana Sílvia <asilvia3@gmail.com>
2018-03-26 14:37:43 +0200

    add data and empty column

--- app/src/main/java/com/android/asilvia/cryptoo/widget/UpdateService.java
+++ app/src/main/java/com/android/asilvia/cryptoo/widget/UpdateService.java
@@ -11,12 +11,14 @@ import android.content.ComponentName;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.view.View;
 import android.widget.RemoteViews;
 
 import com.android.asilvia.cryptoo.R;
 import com.android.asilvia.cryptoo.api.ApiResponse;
 import com.android.asilvia.cryptoo.db.LocalCoin;
 import com.android.asilvia.cryptoo.repository.DataManager;
+import com.android.asilvia.cryptoo.ui.start.StartAdapter;
 import com.android.asilvia.cryptoo.util.AbsentLiveData;
 import com.android.asilvia.cryptoo.vo.CoinsPrice;
 import com.bumptech.glide.Glide;
@@ -74,18 +76,22 @@ public class UpdateService  extends LifecycleService{
                 double index = coinsPriceApiResponse.body.getRAW().get(savedCoins.get(0).getKey()).get(to).getCHANGEPCT24HOUR();
                 savedCoins.get(0).setIndex(index);
                 Timber.d("On changeeeeeee" + price);
-                view.setTextViewText(R.id.coin_name1, savedCoins.get(0).getName());
-
-                AppWidgetTarget appWidgetTarget=
-                new AppWidgetTarget( getApplicationContext(), view, R.id.coin_icon1, 0 );
-
-
-                Glide
-                        .with( getApplicationContext() ) // safer!
-                        .load(savedCoins.get(0).getImageUrl())
-                        .asBitmap()
-                        .into( appWidgetTarget );
-
+                setFirstColumn(view, savedCoins.get(0));
+                setSecondColumn(view, savedCoins.get(1));
+                if(size >= 3) {
+                    view.setViewVisibility(R.id.llThirdEmpty, View.GONE);
+                    view.setViewVisibility(R.id.llThird, View.VISIBLE);
+                    view.setViewVisibility(R.id.llThirdrow, View.VISIBLE);
+                    view.setViewVisibility(R.id.llThirdrowEmpty, View.GONE);
+                    setThirdColumn(view, savedCoins.get(2));
+                }
+                else
+                {
+                    view.setViewVisibility(R.id.llThirdEmpty, View.VISIBLE);
+                    view.setViewVisibility(R.id.llThird, View.GONE);
+                    view.setViewVisibility(R.id.llThirdrow, View.GONE);
+                    view.setViewVisibility(R.id.llThirdrowEmpty, View.VISIBLE);
+                }
 
 
 
@@ -144,6 +150,101 @@ public class UpdateService  extends LifecycleService{
         return super.onStartCommand(intent, flags, startId);
     }
 
+    private void setFirstColumn(RemoteViews view, LocalCoin coin) {
+        view.setTextViewText(R.id.coin_name1, coin.getKey());
+        AppWidgetTarget appWidgetTarget=
+        new AppWidgetTarget( getApplicationContext(), view, R.id.coin_icon1, 0 );
+
+
+        Glide
+                .with( getApplicationContext() ) // safer!
+                .load(coin.getImageUrl())
+                .asBitmap()
+                .into( appWidgetTarget );
+
+        view.setTextViewText(R.id.coin_percentage1, String.format("%.2f",coin.getIndex() ) + "%");
+        view.setImageViewResource(R.id.coin_indicator1, getIndicator(coin.getIndex()));
+        view.setTextViewText(R.id.coin_value1, String.format("%.2f",coin.getPrice() ));
+        view.setTextViewText(R.id.coin_user_profit1, getProfit(coin));
+        view.setTextViewText(R.id.coin_user_profit_indicator1, getProfitIndexText(coin));
+        view.setImageViewResource(R.id.coin_user_profit_indicator_icon1, getIndicator(getProfitIndex(coin)));
+    }
+    private void setSecondColumn(RemoteViews view, LocalCoin coin) {
+        view.setTextViewText(R.id.coin_name2, coin.getKey());
+        AppWidgetTarget appWidgetTarget=
+                new AppWidgetTarget( getApplicationContext(), view, R.id.coin_icon2, 0 );
+
+
+        Glide
+                .with( getApplicationContext() ) // safer!
+                .load(coin.getImageUrl())
+                .asBitmap()
+                .into( appWidgetTarget );
+
+        view.setTextViewText(R.id.coin_percentage2, String.format("%.2f",coin.getIndex() ) + "%");
+        view.setImageViewResource(R.id.coin_indicator2, getIndicator(coin.getIndex()));
+        view.setTextViewText(R.id.coin_value2, String.format("%.2f",coin.getPrice() ));
+        view.setTextViewText(R.id.coin_user_profit2, getProfit(coin));
+        view.setTextViewText(R.id.coin_user_profit_indicator2, getProfitIndexText(coin));
+        view.setImageViewResource(R.id.coin_user_profit_indicator_icon2, getIndicator(getProfitIndex(coin)));
+    }
+
+    private void setThirdColumn(RemoteViews view, LocalCoin coin) {
+        view.setTextViewText(R.id.coin_name3, coin.getKey());
+        AppWidgetTarget appWidgetTarget=
+                new AppWidgetTarget( getApplicationContext(), view, R.id.coin_icon3, 0 );
+
+
+        Glide
+                .with( getApplicationContext() ) // safer!
+                .load(coin.getImageUrl())
+                .asBitmap()
+                .into( appWidgetTarget );
+
+        view.setTextViewText(R.id.coin_percentage3, String.format("%.2f",coin.getIndex() ) + "%");
+        view.setImageViewResource(R.id.coin_indicator3, getIndicator(coin.getIndex()));
+        view.setTextViewText(R.id.coin_value3, String.format("%.2f",coin.getPrice() ));
+        view.setTextViewText(R.id.coin_user_profit3, getProfit(coin));
+        view.setTextViewText(R.id.coin_user_profit_indicator3, getProfitIndexText(coin));
+        view.setImageViewResource(R.id.coin_user_profit_indicator_icon3, getIndicator(getProfitIndex(coin)));
+    }
+
+    private int  getIndicator(double percentage) {
+        if(percentage > 0)
+        {
+           return R.drawable.ic_arrow_drop_up;
+        }
+        else
+        {
+            return R.drawable.ic_arrow_drop_down;
+
+        }
+    }
+    public String getProfit(LocalCoin localCoin)
+    {
+
+        double finalPrice = localCoin.getPrice();
+        double userPrice = localCoin.getUserPrice();
+        return String.format("%.2f",(finalPrice - userPrice));
+    }
+
+
+
+    public String getProfitIndexText(LocalCoin localCoin)
+    {
+
+        double finalPrice = localCoin.getPrice();
+        double userPrice = localCoin.getUserPrice();
+        return String.format("%.2f", ((finalPrice - userPrice)/finalPrice * 100)) + "%";
+    }
+
+    public double getProfitIndex(LocalCoin localCoin)
+    {
+
+        double finalPrice = localCoin.getPrice();
+        double userPrice = localCoin.getUserPrice();
+        return  ((finalPrice - userPrice)/finalPrice * 100);
+    }
 
 
     @Override
