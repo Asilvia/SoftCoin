d7c8ba4fda8a24d32058bae4a551a55936345fd8
asilvia <asilvia3@gmail.com>
2018-04-18 21:13:39 +0200

    fix issue with no connection

--- app/src/main/java/com/android/asilvia/cryptoo/util/NetworkUtils.java
+++ app/src/main/java/com/android/asilvia/cryptoo/util/NetworkUtils.java
@@ -1,8 +1,13 @@
 package com.android.asilvia.cryptoo.util;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+
+import com.android.asilvia.cryptoo.R;
 
 /**
  * Created by asilvia on 26-10-2017.
@@ -19,4 +24,5 @@ public final class NetworkUtils {
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
+
 }
